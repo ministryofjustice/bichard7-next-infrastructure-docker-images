b3c4d5ae233502b7abeef53bd20d8d21f291783a
Brett Minnie <github@bdb-studios.co.uk>
2021-07-07 14:21:38 +0100

    Import of JDK image

---
+++ Openjdk_Jre11_Slim/scripts/build_docker_jre11_slim.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -ev
+
+readonly REPOSITORY_NAME="openjdk-jre11-slim"
+readonly SOURCE_REPOSITORY_NAME="amazon-linux2"
+readonly REPOSITORY="${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com"
+readonly DOCKER_IMAGE_PREFIX="${REPOSITORY}/${REPOSITORY_NAME}"
+readonly SOURCE_IMAGE_PREFIX="${REPOSITORY}/${SOURCE_REPOSITORY_NAME}"
+
+echo "Build openJDK11 Jre image on `date`"
+
+aws ecr get-login-password --region eu-west-2 | docker login \
+  --username AWS \
+  --password-stdin ${REPOSITORY}
+
+DOCKER_IMAGE_HASH=$(aws ecr describe-images --repository-name ${SOURCE_REPOSITORY_NAME} --query \
+  "{\"tags\": to_string(sort_by(imageDetails,& imagePushedAt)[-1].imageDigest)}" | jq .tags | tr -d '"')
+
+echo ${DOCKER_IMAGE_HASH}
+
+docker build --build-arg "BUILD_IMAGE=${SOURCE_IMAGE_PREFIX}@${DOCKER_IMAGE_HASH}" -t ${REPOSITORY_NAME} . 2>&1 | tee /tmp/docker.out
+
+export SHA_HASH=$(cat /tmp/docker.out | grep digest | cut -d':' -f3-4 | cut -d' ' -f2)
+
+docker tag ${REPOSITORY_NAME}:latest ${DOCKER_IMAGE_PREFIX}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}
+echo "Push Docker image on `date`"
+
+docker push ${DOCKER_IMAGE_PREFIX}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}
+
+aws codebuild start-build --project-name "build-prometheus-cloudwatch-exporter-docker" \
+  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
+
+aws codebuild start-build --project-name "build-prometheus-docker" \
+  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
