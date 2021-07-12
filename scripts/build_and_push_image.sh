e8c3ebd3b5079694d6c97271d987d27833bdeaf5
Brett Minnie <github@bdb-studios.co.uk>
2021-07-12 10:33:52 +0100

    Added trivy scan before push

--- scripts/build_and_push_image.sh
+++ scripts/build_and_push_image.sh
@@ -28,6 +28,9 @@ docker build \
   -t ${REPOSITORY_NAME} .
 
 ## Test image steps
+echo "Running trivy tests against ${DOCKER_IMAGE}"
+/bin/bash ../scripts/run_trivy_tests.sh
+echo "Running goss tests against ${DOCKER_IMAGE}"
 /bin/bash ../scripts/run_goss_tests.sh
 
 ## Push image steps
---
+++ scripts/run_trivy_tests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+install_trivy() {
+  echo "Installing trivy binary"
+  TRIVY_VERSION=$(get_latest_release "aquasecurity/trivy" | sed 's/v//')
+  rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.rpm
+}
+
+install_trivy
+
+trivy image ${DOCKER_IMAGE}
