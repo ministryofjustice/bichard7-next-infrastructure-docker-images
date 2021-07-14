ae0dab150c20a70193c1dce4907ff02012887229
Brett Minnie <github@bdb-studios.co.uk>
2021-07-14 16:41:56 +0100

    Updated build script

--- Grafana/scripts/build_docker.sh
+++ Grafana/scripts/build_docker.sh
@@ -3,6 +3,6 @@
 set -e
 
 export readonly REPOSITORY_NAME="grafana"
-export readonly SOURCE_REPOSITORY_NAME="grafana-staged"
+export readonly SOURCE_REPOSITORY_NAME="amazon-linux2-base"
 
 /bin/bash ../scripts/build_and_push_image.sh
