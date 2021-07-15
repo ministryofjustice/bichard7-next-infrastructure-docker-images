5f990eaa26e2dcddd26ac42bd085666c28f559cd
Brett Minnie <github@bdb-studios.co.uk>
2021-07-15 16:22:12 +0100

    Set AlertManagerUrl in goss

--- Grafana/scripts/build_docker.sh
+++ Grafana/scripts/build_docker.sh
@@ -4,5 +4,6 @@ set -e
 
 export readonly REPOSITORY_NAME="grafana"
 export readonly SOURCE_REPOSITORY_NAME="amazon-linux2-base"
+export GOSS_ENV="-e ALERTMANAGER_URL=localhost"
 
 /bin/bash ../scripts/build_and_push_image.sh
