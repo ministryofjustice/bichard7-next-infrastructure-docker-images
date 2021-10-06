800836c89001918646d2d02af4751e3b474d222a
Brett Minnie <github@bdb-studios.co.uk>
2021-10-06 09:21:05 +0100

    Some debug

--- scripts/run_trivy_tests.sh
+++ scripts/run_trivy_tests.sh
@@ -11,6 +11,7 @@ get_latest_release() {
 install_trivy() {
   echo "Installing trivy binary"
   TRIVY_VERSION=$(get_latest_release "aquasecurity/trivy" | sed 's/v//')
+  echo "Installing trivy version ${TRIVY_VERSION}"
   yum install -y https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.rpm
 }
 
