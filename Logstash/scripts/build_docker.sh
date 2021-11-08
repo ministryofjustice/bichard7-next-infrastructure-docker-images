7bdcd1d419466f5003f7021e883513babd416b08
Brett Minnie <github@bdb-studios.co.uk>
2021-11-08 15:55:23 +0000

    Increase AWS timeout

--- Logstash/scripts/build_docker.sh
+++ Logstash/scripts/build_docker.sh
@@ -4,6 +4,6 @@ set -e
 
 export readonly REPOSITORY_NAME="logstash"
 export readonly SOURCE_REPOSITORY_NAME="amazon-linux2-base"
-export readonly GOSS_SLEEP=60
+export readonly GOSS_SLEEP=120
 
 /bin/bash ../scripts/build_and_push_image.sh
