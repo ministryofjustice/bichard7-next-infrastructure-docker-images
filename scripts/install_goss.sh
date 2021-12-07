e057d2c85efaa90dad575142bdf08cf03fcd7698
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2021-12-07 11:27:07 +0000

    fix: 🐛 pull trivy and goss from our s3

--- scripts/install_goss.sh
+++ scripts/install_goss.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# this is ran on circleci
 if [ ! -f "/usr/local/bin/goss" ]; then
   curl -L https://github.com/aelsabbahy/goss/releases/latest/download/goss-linux-amd64 -o /usr/local/bin/goss
   chmod +rx /usr/local/bin/goss
