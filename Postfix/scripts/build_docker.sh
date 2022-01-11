b24a213b7dbdd26b41cb8fae658b39bf694b1801
Brett Minnie <github@bdb-studios.co.uk>
2022-01-11 14:33:11 +0000

    Added postfix builder script

---
+++ Postfix/scripts/build_docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+export readonly REPOSITORY_NAME="postfix"
+export readonly SOURCE_REPOSITORY_NAME="amazon-linux2-base"
+
+export GOSS_ENV=""
+/bin/bash ../scripts/build_and_push_image.sh
