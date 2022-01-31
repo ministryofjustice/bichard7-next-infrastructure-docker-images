f86dafa9ae6a8c8f0ab7ac073431c8b41a3124d7
Brett Minnie <github@bdb-studios.co.uk>
2022-01-31 14:47:57 +0000

    Inverted image references

--- Codebuild_Base/scripts/build_docker.sh
+++ Codebuild_Base/scripts/build_docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export readonly REPOSITORY_NAME="nodejs"
-export readonly SOURCE_REPOSITORY_NAME="codebuild-base"
+export readonly REPOSITORY_NAME="codebuild-base"
+export readonly SOURCE_REPOSITORY_NAME="nodejs"
 
 /bin/bash ../scripts/build_and_push_image.sh
