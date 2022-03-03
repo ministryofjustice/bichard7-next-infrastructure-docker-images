cf9c40b67d0be949f0ce3be687ac707638d8960a
Jaskaran Sarkaria <jaskaran.sarkaria@googlemail.com>
2022-03-03 09:59:43 +0000

    fix: 🐛 get latest terraform version

--- Codebuild_Base/scripts/build_docker.sh
+++ Codebuild_Base/scripts/build_docker.sh
@@ -4,5 +4,7 @@ set -e
 
 export readonly REPOSITORY_NAME="codebuild-base"
 export readonly SOURCE_REPOSITORY_NAME="nodejs"
+LATEST_TERRAFORM_CLI_VERSION=$(curl --silent https://github.com/hashicorp/terraform/releases/latest | grep -Eo 'v.*\"' | sed 's/^v\(.*\)\"$/\1/')
+export BUILD_ARGS="--build-arg TERRAFORM_VERSION=${LATEST_TERRAFORM_CLI_VERSION}"
 
 /bin/bash ../scripts/build_and_push_image.sh
