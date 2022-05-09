33709b1a626e54c88aef8a7ff55073475a39846c
Jaskaran Sarkaria <jaskaransarkaria@economist.com>
2022-05-09 13:58:59 +0100

    fix: 🐛 the way we pull the latest version from github changed (#147)

--- Codebuild_Base/scripts/build_docker.sh
+++ Codebuild_Base/scripts/build_docker.sh
@@ -4,7 +4,7 @@ set -e
 
 export readonly REPOSITORY_NAME="codebuild-base"
 export readonly SOURCE_REPOSITORY_NAME="nodejs"
-LATEST_TERRAFORM_CLI_VERSION=$(curl --silent https://github.com/hashicorp/terraform/releases/latest | grep -Eo 'v.*\"' | sed 's/^v\(.*\)\"$/\1/')
+LATEST_TERRAFORM_CLI_VERSION=$(curl --silent -L https://github.com/hashicorp/terraform/releases/latest -w %\{url_effective\} | tail -1 | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
 export BUILD_ARGS="--build-arg TERRAFORM_VERSION=${LATEST_TERRAFORM_CLI_VERSION}"
 
 /bin/bash ../scripts/build_and_push_image.sh
