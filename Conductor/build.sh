e39eda52efa2ed7aaa3bcfa989e57ac59509420f
Jamie Davies <jamie.davies@madetech.com>
2023-01-05 11:46:58 +0000

    Make codebuild job actually download the conductor repo before building

--- Conductor/build.sh
+++ Conductor/build.sh
@@ -2,33 +2,17 @@
 
 set -ex
 
-CONDUCTOR_VERSION="v3.13.2"
-DOWNLOAD_URL="https://github.com/Netflix/conductor/archive/refs/tags/${CONDUCTOR_VERSION}.tar.gz"
-SCRIPTPATH=$(dirname "$0")
-ARCHIVE_NAME=conductor.tar.gz
-BUILD_DIR=$SCRIPTPATH/conductor
+CONDUCTOR_VERSION=$(cat conductor_version)
 
 IMAGE="conductor:${CONDUCTOR_VERSION}"
 
-download_and_patch_conductor() {
-  if [ ! -d $BUILD_DIR ]
-  then
-    curl -L -o $SCRIPTPATH/$ARCHIVE_NAME $DOWNLOAD_URL
-    mkdir -p $SCRIPTPATH/conductor
-    tar -xzf $SCRIPTPATH/$ARCHIVE_NAME -C $BUILD_DIR --strip-components 1
-    rm $ARCHIVE_NAME
-    patch -u $BUILD_DIR/server/build.gradle -i $SCRIPTPATH/patches/add-postgres-support.patch
-    patch -u $BUILD_DIR/build.gradle -i $SCRIPTPATH/patches/remove-nebula-plugin.patch
-  fi
-}
-
 build_docker(){
   if [ "${PLATFORM}x" = "arm64x" ]; then
-    docker build --platform linux/${PLATFORM} --build-arg CONFD_CHECKSUM=abd4b6a96b8af12a01e6c3063defec2655d5b817a74d43fb706c9ca8f814dd2e --build-arg CONFD_PLATFORM=arm64 --build-arg BUILD_IMAGE=nginx-java-supervisord:arm -t "${IMAGE}-arm" . 
+    docker build --platform linux/${PLATFORM} --build-arg CONFD_CHECKSUM=abd4b6a96b8af12a01e6c3063defec2655d5b817a74d43fb706c9ca8f814dd2e --build-arg CONFD_PLATFORM=arm64 --build-arg BUILD_IMAGE=nginx-java-supervisord:arm -t "${IMAGE}-arm" .
   else
-    docker build -t $IMAGE . 
+    docker build -t $IMAGE .
   fi
 }
 
-download_and_patch_conductor
+/bin/bash scripts/download_and_patch_conductor.sh
 build_docker
