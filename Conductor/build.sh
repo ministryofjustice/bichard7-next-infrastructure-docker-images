b8d88c4ac057d00853f03ae457b9b6cdbac17274
Jamie Davies <jamie.davies@madetech.com>
2023-01-05 11:56:35 +0000

    Refactor conductor build.sh script for clarity

--- Conductor/build.sh
+++ Conductor/build.sh
@@ -3,16 +3,12 @@
 set -ex
 
 CONDUCTOR_VERSION=$(cat conductor_version)
-
 IMAGE="conductor:${CONDUCTOR_VERSION}"
 
-build_docker(){
-  if [ "${PLATFORM}x" = "arm64x" ]; then
-    docker build --platform linux/${PLATFORM} --build-arg CONFD_CHECKSUM=abd4b6a96b8af12a01e6c3063defec2655d5b817a74d43fb706c9ca8f814dd2e --build-arg CONFD_PLATFORM=arm64 --build-arg BUILD_IMAGE=nginx-java-supervisord:arm -t "${IMAGE}-arm" .
-  else
-    docker build -t $IMAGE .
-  fi
-}
-
 /bin/bash scripts/download_and_patch_conductor.sh
-build_docker
+
+if [ "${PLATFORM}x" = "arm64x" ]; then
+  docker build --platform linux/${PLATFORM} --build-arg CONFD_CHECKSUM=abd4b6a96b8af12a01e6c3063defec2655d5b817a74d43fb706c9ca8f814dd2e --build-arg CONFD_PLATFORM=arm64 --build-arg BUILD_IMAGE=nginx-java-supervisord:arm -t "${IMAGE}-arm" .
+else
+  docker build -t $IMAGE .
+fi
