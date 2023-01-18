1908bc864d7bd39f877caaf68e93aa7a70bdfed2
Ian King <ian.antking@gmail.com>
2023-01-18 14:25:31 +0000

    Remove build image override from conductor build script

--- Conductor/build.sh
+++ Conductor/build.sh
@@ -8,7 +8,7 @@ IMAGE="conductor:${CONDUCTOR_VERSION}"
 /bin/bash scripts/download_and_patch_conductor.sh
 
 if [ "${PLATFORM}x" = "arm64x" ]; then
-  docker build --platform linux/${PLATFORM} --build-arg CONFD_CHECKSUM=abd4b6a96b8af12a01e6c3063defec2655d5b817a74d43fb706c9ca8f814dd2e --build-arg CONFD_PLATFORM=arm64 --build-arg BUILD_IMAGE=nginx-java-supervisord:arm -t "${IMAGE}-arm" .
+  docker build --platform linux/${PLATFORM} --build-arg CONFD_CHECKSUM=abd4b6a96b8af12a01e6c3063defec2655d5b817a74d43fb706c9ca8f814dd2e --build-arg CONFD_PLATFORM=arm64 -t "${IMAGE}-arm" .
 else
   docker build -t $IMAGE .
 fi
