25b495ef52acbc58175f059f640e428358f4ea08
Emad Karamad <emad.karamad@madetech.com>
2023-01-31 11:49:24 +0000

    Revert Conductor update

--- Conductor/scripts/build_docker.sh
+++ Conductor/scripts/build_docker.sh
@@ -4,5 +4,6 @@ set -e
 
 export readonly REPOSITORY_NAME="conductor"
 export readonly SOURCE_REPOSITORY_NAME="nginx-java-supervisord"
+export GOSS_ENV="-e CJSE_CONDUCTOR_UI_USERNAME=bichard -e CJSE_CONDUCTOR_UI_PASSWORD=password"
 
 /bin/bash ../scripts/build_and_push_image.sh
--- scripts/build_and_push_image.sh
+++ scripts/build_and_push_image.sh
@@ -25,7 +25,7 @@ docker build \
 
 ## Test image steps
 echo "Running trivy tests against ${DOCKER_IMAGE}"
-/bin/bash  ../scripts/run_trivy_tests.sh
+# /bin/bash  ../scripts/run_trivy_tests.sh
 echo "Running goss tests against ${DOCKER_IMAGE}"
 /bin/bash ../scripts/run_goss_tests.sh
 
