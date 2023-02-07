26f7013f50ddb3d2e796da31d232b22de5cd5567
Jamie Davies <jamie.davies@madetech.com>
2023-02-07 11:35:13 +0000

    Ensure conductor UI username and password are set when running goss

--- Conductor/scripts/build_docker.sh
+++ Conductor/scripts/build_docker.sh
@@ -5,4 +5,6 @@ set -e
 export readonly REPOSITORY_NAME="conductor"
 export readonly SOURCE_REPOSITORY_NAME="nginx-java-supervisord"
 
+export readonly GOSS_ENV="-e CJSE_CONDUCTOR_UI_USERNAME='conductor' -e CJSE_CONDUCTOR_UI_PASSWORD='password'"
+
 /bin/bash ../scripts/build_and_push_image.sh
