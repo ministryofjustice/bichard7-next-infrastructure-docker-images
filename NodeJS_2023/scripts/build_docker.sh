8f8f4d5c3192b882f1b799c378addc7dd006a217
Ian King <ian.antking@gmail.com>
2023-08-15 14:38:54 +0100

    add nodejs-2023 image

---
+++ NodeJS_2023/scripts/build_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+export readonly REPOSITORY_NAME="nodejs-2023"
+export readonly SOURCE_REPOSITORY_NAME="amazon-linux2023-base"
+
+/bin/bash ../scripts/build_and_push_image.sh
+
+aws codebuild start-build --project-name "build-nginx-nodejs-supervisord-docker" \
+  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
+
+aws codebuild start-build --project-name "build-codebuild-base"
