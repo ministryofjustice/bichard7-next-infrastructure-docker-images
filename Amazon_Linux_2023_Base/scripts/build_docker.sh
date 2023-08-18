5b8a4f17ec8b0a62716f1a76207c02096d6e179c
Ian King <ian.antking@gmail.com>
2023-08-18 14:54:55 +0100

    only trigger build jobs for dependant images

--- Amazon_Linux_2023_Base/scripts/build_docker.sh
+++ Amazon_Linux_2023_Base/scripts/build_docker.sh
@@ -7,19 +7,6 @@ export readonly SOURCE_REPOSITORY_NAME="amazon-linux-2023"
 
 /bin/bash ../scripts/build_and_push_image.sh
 
-aws codebuild start-build --project-name "build-openjdk-jre11-slim-docker" \
+aws codebuild start-build --project-name "build-nodejs-2023-docker" \
   --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
 
-aws codebuild start-build --project-name "build-nodejs-16-docker" \
-  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
-
-aws codebuild start-build --project-name "build-logstash-docker" \
-  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
-
-aws codebuild start-build --project-name "build-grafana-ssl-docker"
-
-aws codebuild start-build --project-name "build-grafana-codebuild-ssl-docker"
-
-aws codebuild start-build --project-name "build-nginx-supervisord-docker"
-
-aws codebuild start-build --project-name "build-postfix-docker"
