bf70558343ab50f338b628754216ec2b9284b4cb
Ben Pirt <ben@pirt.co.uk>
2023-09-15 10:23:38 +0100

    Build the Node v20 image

--- NodeJS_2023/scripts/build_docker.sh
+++ NodeJS_2023/scripts/build_docker.sh
@@ -9,4 +9,3 @@ export readonly SOURCE_REPOSITORY_NAME="amazon-linux2023-base"
 
 aws codebuild start-build --project-name "build-nginx-nodejs-2023-supervisord-docker" \
   --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
-
