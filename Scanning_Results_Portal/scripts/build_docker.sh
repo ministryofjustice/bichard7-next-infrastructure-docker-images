e5d90e17a45d28019a7ccf6121bf74f9240d23c7
Ian King <36984875+ian-antking@users.noreply.github.com>
2023-09-26 11:17:41 +0100

    Bicaws-2773: Remove unnecessary vulnerability references  (#265)
    * copy .trivyigore to each image
    
    * remove global .trivyigore
    
    * reference .trivyignore in image directory
    
    * remove conduct vulnerability ignore from non-conductor images
    
    * remove prometheus vulnerability from non-prometheus containers
    
    * remove logstash vulnerabilities from non-logstash containers
    
    * remove maven vulnerability from non-logstash
    
    * remove log4j vulnerability except conductor and logstash
    
    * remove lo4j db vulnerability excep logstash and conductor
    
    * remove cloud foundry vulnerability except conductor
    
    ---------
    
    Co-authored-by: Richard Cane <richard.cane@madetech.com>

--- Scanning_Results_Portal/scripts/build_docker.sh
+++ Scanning_Results_Portal/scripts/build_docker.sh
@@ -6,4 +6,4 @@ export readonly REPOSITORY_NAME="nginx-scan-portal"
 export readonly SOURCE_REPOSITORY_NAME="nginx-nodejs-supervisord"
 
 export GOSS_ENV="-e AWS_ACCESS_KEY_ID='' -e AWS_SECRET_ACCESS_KEY='' -e S3_BUCKET_NAME='dummy' -e HTPASSWD=test"
-/bin/bash ../scripts/build_and_push_image.sh
+/bin/bash ../scripts/_image.sh
