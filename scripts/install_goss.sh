b8e416ecc7fde81b1f32c02db3d1df53c756d7f5
Joe Folkard <joe.folkard@madetech.com>
2023-11-13 16:00:48 +0000

    Added output flag

--- scripts/install_goss.sh
+++ scripts/install_goss.sh
@@ -5,7 +5,7 @@ set -e
 VERSION=v0.3.23
 # this is ran on circleci
 if [ ! -f "/usr/local/bin/goss" ]; then
-  curl -L "https://github.com/goss-org/goss/releases/download/${VERSION}/goss-linux-amd64"
+  curl --output -L "https://github.com/goss-org/goss/releases/download/${VERSION}/goss-linux-amd64"
   chmod +rx /usr/local/bin/goss
 fi
 if [ ! -f "/usr/local/bin/dgoss" ]; then
