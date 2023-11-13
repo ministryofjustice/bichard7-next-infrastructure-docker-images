1428a1128714e1d75c5a6b4425d4898ba5940115
Joe Folkard <joe.folkard@madetech.com>
2023-11-13 16:27:55 +0000

    undo fixed version of goss

--- scripts/install_goss.sh
+++ scripts/install_goss.sh
@@ -4,7 +4,7 @@ set -e
 
 # this is ran on circleci
 if [ ! -f "/usr/local/bin/goss" ]; then
-  curl -L "https://github.com/goss-org/goss/releases/v0.3.23/download/goss-linux-amd64" -o /usr/local/bin/goss
+  curl -L "https://github.com/goss-org/goss/releases/latest/download/goss-linux-amd64" -o /usr/local/bin/goss
   chmod +rx /usr/local/bin/goss
 fi
 if [ ! -f "/usr/local/bin/dgoss" ]; then
