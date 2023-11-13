a6958e4a6135471e5d3fb9b386616134083d104f
Joe Folkard <joe.folkard@madetech.com>
2023-11-13 15:55:43 +0000

    Added VERSION var

--- scripts/install_goss.sh
+++ scripts/install_goss.sh
@@ -2,16 +2,13 @@
 
 set -e
 
+VERSION=v0.3.23
 # this is ran on circleci
 if [ ! -f "/usr/local/bin/goss" ]; then
-  curl -L https://github.com/aelsabbahy/goss/releases/v0.3.23/download/goss-linux-amd64 -o /usr/local/bin/goss
+  curl -L "https://github.com/goss-org/goss/releases/download/${VERSION}/goss-linux-amd64"
   chmod +rx /usr/local/bin/goss
-
-  goss -v
 fi
 if [ ! -f "/usr/local/bin/dgoss" ]; then
   curl -L https://github.com/aelsabbahy/goss/releases/latest/download/dgoss -o /usr/local/bin/dgoss
   chmod +rx /usr/local/bin/dgoss
-
-  goss -v
 fi
