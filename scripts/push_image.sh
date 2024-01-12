06f42354c00f938ce4852fafe2192734ba46d970
Ben Pirt <ben@pirt.co.uk>
2024-01-12 15:51:48 +0000

    Re-work the build of Conductor

---
+++ scripts/push_image.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ex
+
+## Build image steps
+export readonly REPOSITORY="${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com"
+export readonly DOCKER_IMAGE_PREFIX="${REPOSITORY}/${REPOSITORY_NAME}"
+export readonly SOURCE_IMAGE_PREFIX="${REPOSITORY}/${SOURCE_REPOSITORY_NAME}"
+export readonly DOCKER_IMAGE="${REPOSITORY_NAME}:latest"
+
+## Push image steps
+docker tag ${REPOSITORY_NAME}:latest ${DOCKER_IMAGE_PREFIX}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}
+
+echo "Pushing ${REPOSITORY_NAME} image on `date`"
+docker push ${DOCKER_IMAGE_PREFIX}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}  | tee /tmp/docker.out
+export IMAGE_SHA_HASH=$(cat /tmp/docker.out | grep digest | cut -d':' -f3-4 | cut -d' ' -f2)
+
+if [ "${IS_CD}" = "true" ]; then
+  cat <<EOF>/tmp/${REPOSITORY_NAME}.json
+  {
+    "source-hash" : "${CODEBUILD_RESOLVED_SOURCE_VERSION}",
+    "build-time": "${CODEBUILD_START_TIME}",
+    "image-hash": "${IMAGE_SHA_HASH}"
+  }
+EOF
+  aws s3 cp --quiet /tmp/${REPOSITORY_NAME}.json s3://${ARTIFACT_BUCKET}/semaphores/${REPOSITORY_NAME}.json
+  export "$(echo "${REPOSITORY_NAME}" | tr [:lower:] [:upper:] | tr '-' '_')_HASH"="${IMAGE_SHA_HASH}"
+fi
