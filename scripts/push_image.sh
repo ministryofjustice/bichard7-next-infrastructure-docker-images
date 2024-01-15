dae7d98bfcc383c55055382dedc54cdcc5cbf906
Richard Cane <richard.cane@madetech.com>
2024-01-15 12:29:44 +0000

    Add the ecr docker login to push_image.sh

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -11,6 +11,11 @@ export readonly DOCKER_IMAGE="${REPOSITORY_NAME}:latest"
 ## Push image steps
 docker tag ${REPOSITORY_NAME}:latest ${DOCKER_IMAGE_PREFIX}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}
 
+aws ecr get-login-password --region eu-west-2 | docker login \
+  --username AWS \
+  --password-stdin \
+  "${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com"
+
 echo "Pushing ${REPOSITORY_NAME} image on `date`"
 docker push ${DOCKER_IMAGE_PREFIX}:${CODEBUILD_RESOLVED_SOURCE_VERSION}-${CODEBUILD_START_TIME}  | tee /tmp/docker.out
 export IMAGE_SHA_HASH=$(cat /tmp/docker.out | grep digest | cut -d':' -f3-4 | cut -d' ' -f2)
