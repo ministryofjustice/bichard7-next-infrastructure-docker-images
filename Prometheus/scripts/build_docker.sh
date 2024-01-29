42e039c1bf513502867ea32d572cbe8d66d14ea7
Ben Pirt <ben@pirt.co.uk>
2024-01-29 14:47:07 +0000

    Remove monitoring docker images

--- Prometheus/scripts/build_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-export readonly REPOSITORY_NAME="prometheus"
-export readonly SOURCE_REPOSITORY_NAME="nginx-java-supervisord"
-
-export GOSS_ENV="-e RETENTION_DAYS=1  -e HTPASSWD=test"
-/bin/bash ../scripts/build_and_push_image.sh
