6d7371938e218267930aff29855f1e21af225bd8
Ben Pirt <ben@pirt.co.uk>
2024-03-21 08:46:52 +0000

    Trigger dependent builds

--- Amazon_Linux_2023_Base/scripts/build_docker.sh
+++ Amazon_Linux_2023_Base/scripts/build_docker.sh
@@ -7,6 +7,8 @@ export readonly SOURCE_REPOSITORY_NAME="amazon-linux-2023"
 
 /bin/bash ../scripts/build_and_push_image.sh
 
-aws codebuild start-build --project-name "build-nodejs-2023-docker" \
+aws codebuild start-build --project-name "build-nodejs-20-2023-docker" \
   --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
 
+aws codebuild start-build --project-name "build-nginx-auth-proxy-docker" \
+  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
