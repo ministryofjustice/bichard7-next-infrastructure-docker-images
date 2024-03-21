e2a0205862b2ca824ad377f49d1c2c3844aa00d9
Ben Pirt <ben@pirt.co.uk>
2024-03-21 11:26:46 +0000

    Don't trigger the build-nginx-supervisord-docker build any more

--- Amazon_Linux_Base/scripts/build_docker.sh
+++ Amazon_Linux_Base/scripts/build_docker.sh
@@ -10,6 +10,4 @@ export readonly SOURCE_REPOSITORY_NAME="amazon-linux2"
 aws codebuild start-build --project-name "build-nodejs-16-docker" \
   --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
 
-aws codebuild start-build --project-name "build-nginx-supervisord-docker"
-
 aws codebuild start-build --project-name "build-postfix-docker"
