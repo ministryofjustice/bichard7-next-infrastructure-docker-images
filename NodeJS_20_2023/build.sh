b59f787d81f2f0d94703ca1b47fe2bbf5af36464
Richard Race <1273965+richrace@users.noreply.github.com>
2024-03-26 10:41:56 +0000

    Make sure the arch type is correct

--- NodeJS_20_2023/build.sh
+++ NodeJS_20_2023/build.sh
@@ -4,9 +4,10 @@ set -e
 
 IMAGE="nodejs-20-2023"
 source $(dirname $0)/scripts/node_version.sh
+ARCH=$(arch)
 
 echo "Building Node version: ${NODE_VERSION}"
-docker build $BUILD_ARGS --build-arg NODE_VERSION=$NODE_VERSION -t $IMAGE .
+docker build $BUILD_ARGS --build-arg NODE_VERSION=$NODE_VERSION --build-arg ARCH=$ARCH -t $IMAGE .
 
 if [ "$SKIP_GOSS" = "true" ]; then
   echo "Skipping dgoss tests"
