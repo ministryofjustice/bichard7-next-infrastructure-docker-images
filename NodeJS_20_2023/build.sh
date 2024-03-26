53d49c0ae4dd23b53e7641b2db2931b712eb3787
Richard Race <1273965+richrace@users.noreply.github.com>
2024-03-26 11:32:40 +0000

    Correct the arch

--- NodeJS_20_2023/build.sh
+++ NodeJS_20_2023/build.sh
@@ -6,8 +6,13 @@ IMAGE="nodejs-20-2023"
 source $(dirname $0)/scripts/node_version.sh
 ARCH=$(arch)
 
-echo "Building Node version: ${NODE_VERSION}"
-docker build $BUILD_ARGS --build-arg NODE_VERSION=$NODE_VERSION --build-arg ARCH=$ARCH -t $IMAGE .
+echo "Building Node version: ${NODE_VERSION} and for $ARCH"
+
+if [ "$ARCH" = "arm64" ]; then
+  docker build $BUILD_ARGS --build-arg NODE_VERSION=$NODE_VERSION --build-arg ARCH=arm64 -t $IMAGE .
+else
+  docker build $BUILD_ARGS --build-arg NODE_VERSION=$NODE_VERSION --build-arg ARCH=x64 -t $IMAGE .
+fi
 
 if [ "$SKIP_GOSS" = "true" ]; then
   echo "Skipping dgoss tests"
