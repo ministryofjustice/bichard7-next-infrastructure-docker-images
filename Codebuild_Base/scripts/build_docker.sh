08ae9dfc78fe0a1308816ff4683118256dd80417
joe-fol <joefolkard@protonmail.com>
2024-04-30 16:06:22 +0100

    Bump terraform version to fix go package vulnerability

--- Codebuild_Base/scripts/build_docker.sh
+++ Codebuild_Base/scripts/build_docker.sh
@@ -5,7 +5,7 @@ set -e
 export readonly REPOSITORY_NAME="codebuild-base"
 export readonly SOURCE_REPOSITORY_NAME="amazon-linux2"
 
-LATEST_TERRAFORM_CLI_VERSION="1.4.4"
+LATEST_TERRAFORM_CLI_VERSION="1.8.2"
 export BUILD_ARGS="--build-arg TERRAFORM_VERSION=${LATEST_TERRAFORM_CLI_VERSION}"
 
 /bin/bash ../scripts/build_and_push_image.sh
