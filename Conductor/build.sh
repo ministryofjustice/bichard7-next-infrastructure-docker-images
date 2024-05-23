a5a26222e0d91ef5c2a6d66a81cb62e1864eff7e
Ben Pirt <ben@pirt.co.uk>
2024-05-23 11:05:43 +0100

    Refactor the auth proxy to use envsubst instead of confd

--- Conductor/build.sh
+++ Conductor/build.sh
@@ -8,10 +8,4 @@ if [ -z "$PLATFORM" ]; then
   PLATFORM=$(arch)
 fi
 
-echo "Building conductor for $PLATFORM"
-
-if [ "$PLATFORM" = "arm64" ]; then
-  docker build -f Dockerfile --platform linux/arm64 --build-arg CONDUCTOR_VERSION=$(cat conductor_version) -t $IMAGE .
-else
-  docker build -f Dockerfile --platform linux/amd64 --build-arg CONDUCTOR_VERSION=$(cat conductor_version) -t $IMAGE .
-fi
+docker build --build-arg CONDUCTOR_VERSION=$(cat conductor_version) -t $IMAGE .
