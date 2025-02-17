35ca60af5bad8324e6d6487667101645137f5400
Emad Karamad <emad.karamad@madetech.com>
2025-02-17 17:19:56 +0000

    Install chrome and terraform in amazon linux 2023 base image

--- Amazon_Linux_2023_Base/build.sh
+++ Amazon_Linux_2023_Base/build.sh
@@ -4,7 +4,15 @@ set -e
 
 IMAGE="amazon-linux2023-base"
 
-docker build -t "${IMAGE}" . 
+if [ -z "$PLATFORM" ]; then
+  PLATFORM=$(arch)
+fi
+
+if [ "$PLATFORM" = "arm64" ]; then
+  docker build --platform linux/x86_64 -t $IMAGE .
+else
+  docker build -t "${IMAGE}" . 
+fi
 
 if [ "$SKIP_GOSS" = "true" ]; then
   echo "Skipping dgoss tests"
