0249f2af45c11039de2a089d464434f988fd8b9e
joe-fol <joefolkard@protonmail.com>
2025-12-10 12:15:11 +0000

    Add back old folders for 2 step deployment

---
+++ Nginx_NodeJS_20_2023_Supervisord/scripts/build_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+export readonly REPOSITORY_NAME="nginx-nodejs-20-2023-supervisord"
+export readonly SOURCE_REPOSITORY_NAME="nodejs-20-2023"
+
+/bin/bash ../scripts/build_and_push_image.sh
+
+aws codebuild start-build --project-name "build-s3-web-proxy" \
+  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
+
+aws codebuild start-build --project-name "build-core-repo-artifacts" \
+  --environment-variables-override name=DOCKER_IMAGE_HASH,value="${DOCKER_IMAGE_PREFIX}@${SHA_HASH}",type=PLAINTEXT
---
+++ NodeJS_20_2023/scripts/node_version.sh
@@ -0,0 +1,1 @@
+export NODE_VERSION="20.18.0"
