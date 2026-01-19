b5c7ff3364277bab8770bd3459701dc93ef85da1
Tom Watkins <tom.watkins@justice.gov.uk>
2026-01-19 19:28:48 +0000

    Use GOSS_WAIT_OPTS

--- Conductor/scripts/build_docker.sh
+++ Conductor/scripts/build_docker.sh
@@ -4,7 +4,6 @@ set -e
 
 export readonly REPOSITORY_NAME="conductor"
 export readonly DOCKER_IMAGE="conductor"
-export GOSS_RETRY_TIMEOUT=120
 
 bash ../scripts/install_goss.sh
 make -C ../ Conductor
