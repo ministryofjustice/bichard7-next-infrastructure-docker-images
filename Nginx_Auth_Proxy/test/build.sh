1e7f4dcf6c4485ec224f8da3c2192df05f696240
Tom Watkins <tom.watkins@justice.gov.uk>
2026-06-03 22:02:21 +0100

    Remove incorrect arg usage

--- Nginx_Auth_Proxy/test/build.sh
+++ Nginx_Auth_Proxy/test/build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-BUILD_IMAGE="amazon-linux2023-base:${DOCKER_TAG:-latest}"
 IMAGE="nginx-auth-test"
 
 if [ -z "$PLATFORM" ]; then
@@ -12,7 +11,7 @@ fi
 echo "Building for $PLATFORM"
 
 if [ "$PLATFORM" = "arm64" ]; then
-  docker build --platform linux/arm64 --build-arg BUILD_IMAGE="${BUILD_IMAGE}" -t $IMAGE .
+  docker build --platform linux/arm64 -t $IMAGE .
 else
-  docker build --platform linux/amd64 --build-arg BUILD_IMAGE="${BUILD_IMAGE}" -t $IMAGE .
+  docker build --platform linux/amd64 -t $IMAGE .
 fi
